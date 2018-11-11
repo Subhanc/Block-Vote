141fef5bfd8a4ae702c219800adb75683a05bd3c
Subhanc <schaud86@uwo.ca>
2018-11-11 04:58:55 -0500

    fixed jquery get request

--- authorization.js
+++ authorization.js
@@ -1,10 +1,14 @@
-$('.generate-passphrase').click(function() {
-    $.ajax({
-        type: "GET",
-        url: "https://jsonplaceholder.typicode.com/todos/1",
-        success: function(data) {
-            console.log(data);
-            $('.passphrase').html(data);
-        }
+$(document).ready(function(){
+    $('.generate-passphrase').click(function() {
+        alert(1);
+        $.ajax({
+            type: "GET",
+            url: "https://reqres.in/api/users?page=2",
+            success: function(data) {
+                console.log(data);
+                //$('.passphrase').html(data);
+            }
+        });
     });
 });
+
