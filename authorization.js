cd435d3adb727a7e38597309c2dbe650f43d552b
Subhanc <schaud86@uwo.ca>
2018-11-11 04:39:21 -0500

    test url

--- authorization.js
+++ authorization.js
@@ -1,7 +1,7 @@
 $('.generate-passphrase').click(function() {
     $.ajax({
         type: "GET",
-        url: "http://localhost:5000/",
+        url: "https://jsonplaceholder.typicode.com/todos/1",
         success: function(data) {
             console.log(data);
             $('.passphrase').html(data);
